9204f14bc6d77c907eea2b2e2fc8dcf4e2de4890
Bessou Frank <fbessou@free.fr>
2017-01-31 18:09:22 +0100

    Merged inputs and made event loop.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """ GAME """
 import sys, pygame
 from time import sleep
+from player_inputs import PlayerInputs
 pygame.init()
 
 SIZE = width, height = 320, 240
@@ -12,19 +13,46 @@ screen = pygame.display.set_mode(SIZE)
 x = 19
 y = 20
 s = 17
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-    
-    x+=SPEED[0]
-    y+=SPEED[1]
-    if x-s< 0 or x+s > width:
-        SPEED[0] = -SPEED[0]
-    if y-s < 0 or y+s > height:
-        SPEED[1] = -SPEED[1]
 
-    screen.fill(BLACK)
-    pygame.draw.circle(screen, BLUE, [x,y], s)
-    pygame.display.flip()
-    sleep(0.016)
+class RoundState(object):
+    def __init__(self, player_inputs):
+        self.player_inputs = player_inputs
+        self.player1_color = 16, 32, 53
+        self.player2_color = 112, 125, 32
     
+    def loop(self):
+        while 1:
+            self._update()
+            self._render()
+            sleep(0.016)
+
+
+    def _update(self):
+        actions = self.player_inputs.read()
+        for action in actions:
+            print(action)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+        
+        #x+=SPEED[0]
+        #y+=SPEED[1]
+        #if x-s< 0 or x+s > width:
+        #    SPEED[0] = -SPEED[0]
+        #if y-s < 0 or y+s > height:
+        #    SPEED[1] = -SPEED[1]
+        
+    def _render(self):
+        self._clear()
+        #pygame.draw.circle(screen, BLUE, [x,y], s)
+        #pygame.display.flip()
+
+    def _clear(self):
+        screen.fill(BLACK)
+
+def main():
+    player_inputs = PlayerInputs() 
+    game = RoundState(player_inputs)
+    game.loop()
+if __name__ == "__main__":
+    main()
--- player_inputs.py
+++ player_inputs.py
@@ -2,7 +2,7 @@ import pygame
 
 class PlayerInputs:
     def __init__(self):
-        mapping = [
+        self._mapping = [
                 {pygame.K_d:"right", pygame.K_s:"down", pygame.K_q:"left"},
                 {pygame.K_RIGHT:"right", pygame.K_DOWN:"down", pygame.K_LEFT:"left"}
                 ]
@@ -11,11 +11,15 @@ class PlayerInputs:
         inputs = []
         for event in pygame.event.get():
             keydown = False
+            used = False
             if event.type == pygame.KEYDOWN:
                 keydown = True
             if event.type == pygame.KEYUP or keydown:
-                for player in range(len(mapping)):
-                    if event.key in mapping[player]:
-                        inputs.append((player, mapping[player][event.key], keydown))
+                for player in range(len(self._mapping)):
+                    if event.key in self._mapping[player]:
+                        inputs.append((player, self._mapping[player][event.key], keydown))
+                        used = True
+            if not used:
+                pygame.event.post(event)
 
         return inputs
