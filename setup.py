5a3a013f6c5e20ba3d3977c6b6261f95c5a83d80
Bessou Frank <fbessou@free.fr>
2017-05-03 00:20:25 +0200

    Add configuration in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
+import os
 from setuptools import Extension, setup, find_packages
+
+def read(fname):
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='SNAKIS',
       packages=['snakis'],
-        )
+      classifiers=[
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Topic :: Games/Entertainment'
+      ],
+      long_description=read('README.md')
+)
